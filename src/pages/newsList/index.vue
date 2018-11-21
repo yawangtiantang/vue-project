79710006bc17a212c4357644ee70443ec0789244
zhouxu <yawang@163.com>
2018-11-21 21:51:45 +0800

    完成新闻列表页面布局

--- src/pages/newsList/index.vue
+++ src/pages/newsList/index.vue
@@ -1,12 +1,62 @@
 <template>
     <div class="news-list">
-       新闻列表组件
+        <ul class="mui-table-view">
+				<li class="mui-table-view-cell mui-media">
+					<a href="javascript:;">
+						<img class="mui-media-object mui-pull-left" src="https://avatars0.githubusercontent.com/u/38232528?s=400&v=4">
+						<div class="mui-media-body">
+							<h1>幸福</h1>
+							<p class='mui-ellipsis'>
+                                <span>发表时间:2012-12-12 12:12:12</span>
+                                <span>点击:0次</span>
+                            </p>
+						</div>
+					</a>
+				</li>
+				<li class="mui-table-view-cell mui-media">
+					<a href="javascript:;">
+						<img class="mui-media-object mui-pull-left" src="https://avatars0.githubusercontent.com/u/38232528?s=400&v=4">
+						<div class="mui-media-body">
+							<h1>幸福</h1>
+							<p class='mui-ellipsis'>
+                                <span>发表时间:2012-12-12 12:12:12</span>
+                                <span>点击:0次</span>
+                            </p>
+						</div>
+					</a>
+				</li>
+				<li class="mui-table-view-cell mui-media">
+					<a href="javascript:;">
+						<img class="mui-media-object mui-pull-left" src="https://avatars0.githubusercontent.com/u/38232528?s=400&v=4">
+						<div class="mui-media-body">
+							<h1>幸福</h1>
+							<p class='mui-ellipsis'>
+                                <span>发表时间:2012-12-12 12:12:12</span>
+                                <span>点击:0次</span>
+                            </p>
+						</div>
+					</a>
+				</li>
+
+			</ul>
     </div>
 
 </template>
 <script>
     
 </script>
-<style>
-
+<style lang='less'>
+    .mui-table-view{
+        li{
+            h1{
+                font-size:16px;
+            }
+            .mui-ellipsis{
+                font-size:14px;
+                color:skyblue;
+                display:flex;
+                justify-content: space-between;
+            }
+        }
+    }
 </style>
