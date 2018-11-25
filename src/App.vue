2369b03b0865fb751064fe47aa63e9265637066e
zhouxu <yawang@163.com>
2018-11-25 20:43:47 +0800

    解决滑动条带来的所有bug

--- src/App.vue
+++ src/App.vue
@@ -4,19 +4,19 @@
     <mt-header fixed title="vue project"></mt-header>
      <!-- 底部 -->
     <nav class="mui-bar mui-bar-tab">
-			<router-link class="mui-tab-item " to="/home">
+			<router-link class="my-mui-tab-item " to="/home">
 				<span class="mui-icon mui-icon-home"></span>
 				<span class="mui-tab-label">首页</span>
 			</router-link>
-			<router-link class="mui-tab-item" to="/member">
+			<router-link class="my-mui-tab-item" to="/member">
 				<span class="mui-icon mui-icon-email"></span>
 				<span class="mui-tab-label">会员</span>
 			</router-link>
-			<router-link class="mui-tab-item" to="/shopcar">
+			<router-link class="my-mui-tab-item" to="/shopcar">
 				<span class="mui-icon mui-icon-extra mui-icon-extra-cart"><span class="mui-badge">9</span></span>
 				<span class="mui-tab-label">购物车</span>
 			</router-link>
-			<router-link class="mui-tab-item" to="/search">
+			<router-link class="my-mui-tab-item" to="/search">
 				<span class="mui-icon mui-icon-search"></span>
 				<span class="mui-tab-label">搜索</span>
 			</router-link>
@@ -53,5 +53,32 @@
 		.v-enter-active,.v-leave-active{
 			transition:all 0.6s ease
 		}
+		.mui-bar-tab .my-mui-tab-item.mui-active {
+    color: #007aff;
+  }
+  .mui-bar-tab .my-mui-tab-item {
+    display: table-cell;
+    overflow: hidden;
+    width: 1%;
+    height: 50px;
+    text-align: center;
+    vertical-align: middle;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    color: #929292;
+  }
+  .mui-bar-tab .my-mui-tab-item .mui-icon {
+    top: 3px;
+    width: 24px;
+    height: 24px;
+    padding-top: 0;
+    padding-bottom: 0;
+  }
+  .mui-bar-tab .my-mui-tab-item .mui-icon ~ .mui-tab-label {
+    font-size: 11px;
+    display: block;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
   }
 </style>
--- src/pages/photoList/index.vue
+++ src/pages/photoList/index.vue
@@ -18,9 +18,9 @@
 					<a class="mui-control-item" href="#item5mobile" data-wid="tab-top-subpage-5.html">
 							娱乐
 					</a>
-					<!--<a class="mui-control-item" href="#item6mobile" data-wid="tab-top-subpage-6.html">
+					<a class="mui-control-item" href="#item6mobile" data-wid="tab-top-subpage-6.html">
 						科技
-					</a>-->
+					</a>
 				</div>
 			</div>
 
@@ -28,8 +28,30 @@
     </div>
 </template>
 <script>
-    
+// arguments callee caller在严格模式下无法使用 而webpack默认是采用严格模式打包的
+// 所以mui.js无法被打包编译
+// 解决方案: 在 .babelrc 文件中配置忽略, 将mui的js文件全部忽略掉
+import mui from "../../lib/mui/js/mui.min.js";
+
+  
+    export default{
+        data(){
+            return{
+
+            }
+        },
+        methods:{
+
+        },
+		mounted() {
+    // 初始化区域滚动一定要放在mounted钩子函数中
+    // 因为初始化操作必须要等DOM元素加载完成后才可以进行
+    		mui(".mui-scroll-wrapper").scroll({
+     	 deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+    	});
+  	}
+		}
 </script>
 <style lang='less'>
-
+	
 </style>
